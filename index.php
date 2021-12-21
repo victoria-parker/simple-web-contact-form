8b4b9439b1db83c29b24007a33ad58fd2aa0f171
victoria-parker <victoriaparkerb@gmail.com>
2021-12-21 12:49:38 +0000

    form css finished and I add Summernote to make textarea WYSIWYG

--- index.php
+++ index.php
@@ -5,7 +5,18 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>Simple web contact form</title>
+
+    <!-- include libraries(jQuery, bootstrap) -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <!-- include summernote css/js -->
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -14,34 +25,40 @@
     <div id="container">
         <form action="" method="POST">
 
+            <div class="form-el">
             <label for="userName">UserName: </label>
-            <input type="text" name="userName">
+            <input type="text" name="userName" required>
+            </div>
 
-            <br>
-
+            <div class="form-el">
             <label for="userEmail">Email: </label>
-            <input type="email" name="userEmail">
+            <input type="email" name="userEmail" required>
+            </div>
 
-            <br>
-
-            <label for="issue">Select the issue</label>
-            <select name="issue" id="issue">
-                <option value="">Select issue</option>
+            <div class="form-el">
+            <label for="issue">Select the Issue</label>
+            <select name="issue" id="issue" required>
+                <option value="Other" selected>Other</option>
                 <option value="Query">Query</option>
                 <option value="Feedback">Feedback</option>
                 <option value="Complaint">Complaint</option>
-                <option value="Other">Other</option>
             </select>
+            </div>
 
-            <br>
-
+            <div class="form-el">
             <label for="comments">Comments</label>
-            <textarea name="comments" id="comments" cols="30" rows="10"></textarea>
+            <textarea name="comments"  id="summernote"></textarea>
+            </div>
 
-            <br>
-
-            <button>Submit</button>
+            <div class="form-el">
+            <button id="submit">Submit</button>
+            </div>
         </form>
     </div>
+    <script>
+        $(document).ready(function() {
+            $('#summernote').summernote();
+        });
+    </script>
 </body>
 </html>
